db4c5859369a5fb644492e3965e54adad526c032
Bima Adhitya Sukoco <130433831+BimaAdhityaS@users.noreply.github.com>
2024-06-04 14:21:56 +0700

    QoL

--- app/src/main/java/com/example/hoothub/activity/MainActivity.java
+++ app/src/main/java/com/example/hoothub/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.hoothub;
+package com.example.hoothub.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.hoothub.R;
 
 public class MainActivity extends AppCompatActivity {
 
